2bd30f7f9860a5d1eca6cdfac923bb2e81117337
Jacob Allen <jballen95173@gmail.com>
2018-09-21 13:55:34 -0400

    Added post login screen with welcome text based on logged in username and logout button which returns to the welcome screen
    Signed-off-by: Jacob Allen <jballen95173@gmail.com>

--- app/src/main/java/a5x/cs2340/donationtracker/LoginActivity.java
+++ app/src/main/java/a5x/cs2340/donationtracker/LoginActivity.java
@@ -3,6 +3,7 @@ package a5x.cs2340.donationtracker;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -19,6 +20,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -51,6 +53,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "user"
     };
+    public static final String LOGGED_IN_USERNAME = "donationTracker.successfulUsername";
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
@@ -94,48 +97,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mProgressView = findViewById(R.id.login_progress);
     }
 
-    private void populateAutoComplete() {
-        if (!mayRequestContacts()) {
-            return;
-        }
 
-        getLoaderManager().initLoader(0, null, this);
-    }
 
-    private boolean mayRequestContacts() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return true;
-        }
-        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        }
-        if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
-            Snackbar.make(mUsernameView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-                    .setAction(android.R.string.ok, new View.OnClickListener() {
-                        @Override
-                        @TargetApi(Build.VERSION_CODES.M)
-                        public void onClick(View v) {
-                            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-                        }
-                    });
-        } else {
-            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-        }
-        return false;
-    }
-
-    /**
-     * Callback received when a permissions request has been completed.
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_READ_CONTACTS) {
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                populateAutoComplete();
-            }
-        }
-    }
 
 
     /**
@@ -317,15 +280,13 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 // Simulate network access.
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
+                Log.d("test", "oops");
                 return false;
             }
 
             for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mUsername)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
+                //FIX THIS LATER WITH ACTUAL AUTHENTICATION
+                return true;
             }
 
             // TODO: register the new account here.
@@ -336,9 +297,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         protected void onPostExecute(final Boolean success) {
             mAuthTask = null;
             showProgress(false);
-
+            Log.d("test", "On PostExecute with success = " + success);
             if (success) {
-                finish();
+                Log.d("test", "Attempting to go to PostLogin");
+                goToPostLogin(mUsername);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
@@ -350,6 +312,12 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             mAuthTask = null;
             showProgress(false);
         }
+    }
+
+    protected void goToPostLogin(String username) {
+        Intent goToPostLoginIntent = new Intent(this, PostLoginActivity.class);
+        goToPostLoginIntent.putExtra(LOGGED_IN_USERNAME, username);
+        startActivity(goToPostLoginIntent);
     }
 }
 
---
+++ app/src/main/java/a5x/cs2340/donationtracker/PostLoginActivity.java
@@ -0,0 +1,33 @@
+package a5x.cs2340.donationtracker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class PostLoginActivity extends AppCompatActivity {
+    protected String username;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_login);
+        Intent loginIntent = getIntent();
+        username = loginIntent.getStringExtra(LoginActivity.LOGGED_IN_USERNAME);
+        TextView postLoginTextView = findViewById(R.id.postLoginTextView);
+        postLoginTextView.setText("Welcome " + username);
+        Button logoutButton = findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logoutBackToWelcome();
+            }
+        });
+    }
+    protected void logoutBackToWelcome() {
+        username = null;
+        Intent backToWelcomeIntent = new Intent(this, WelcomeActivity.class);
+        startActivity(backToWelcomeIntent);
+    }
+}
