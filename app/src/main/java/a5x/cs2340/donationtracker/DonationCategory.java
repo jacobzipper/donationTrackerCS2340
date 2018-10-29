655cf38abcf7a2e2cb388326473a16277ca4fbf1
Maxim Salikhov <msalihov@gmail.com>
2018-10-29 09:57:06 -0400

    Fixed donations listing

--- app/src/main/java/a5x/cs2340/donationtracker/DonationCategory.java
+++ app/src/main/java/a5x/cs2340/donationtracker/DonationCategory.java
@@ -1,14 +1,22 @@
 package a5x.cs2340.donationtracker;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Enum representing the category the donation falls in
  */
 public enum DonationCategory {
+    @SerializedName("Clothing")
     CLOTHING("Clothing"),
+    @SerializedName("Hat")
     HAT("Hat"),
+    @SerializedName("Kitchen")
     KITCHEN("Kitchen"),
+    @SerializedName("Electronics")
     ELECTRONICS("Electronics"),
+    @SerializedName("Household")
     HOUSEHOLD("Household"),
+    @SerializedName("Other")
     OTHER("Other"),
     NOCAT("<No Category>");
     private final String name;
