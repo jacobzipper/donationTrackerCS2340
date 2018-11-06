421d2b37cb533ed5ca0db8107be9c1ca53407c46
Maxim Salikhov <msalihov@gmail.com>
2018-11-06 15:18:24 -0500

    Fix a typo

--- app/src/main/java/a5x/cs2340/donationtracker/webservice/responses/GetLocationsResponse.java
+++ app/src/main/java/a5x/cs2340/donationtracker/webservice/responses/GetLocationsResponse.java
@@ -3,7 +3,7 @@ package a5x.cs2340.donationtracker.webservice.responses;
 import a5x.cs2340.donationtracker.webservice.responses.responseobjects.Location;
 
 /**
- * Webeservice response for getting the list of locations
+ * Webservice response for getting the list of locations
  */
 public class GetLocationsResponse extends StandardResponse {
     private Location[] locations;
