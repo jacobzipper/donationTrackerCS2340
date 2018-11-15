38ca9701b6ae2c1704baf0bf59bb02cff4868ff6
Jacob Allen <jballen95173@gmail.com>
2018-11-15 00:06:01 -0500

    Added name to test and JUnit to classpath
    Signed-off-by: Jacob Allen <jballen95173@gmail.com>

--- app/src/test/java/a5x/cs2340/donationTracker/SearchDonationsMapTest.java
+++ app/src/test/java/a5x/cs2340/donationTracker/SearchDonationsMapTest.java
@@ -8,6 +8,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
+/**
+ * @author Jacob Allen
+ */
 @SuppressWarnings("ALL")
 public class SearchDonationsMapTest {
     private SearchDonationsMap searchDonationsMap;
